a159195173e9db7a9db6a01f9084ff77cdb0e90c
john gorter <john.gorter@gmail.com>
2015-06-24 16:11:49 +0200

    Complete

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var fs = require('fs');
 var express = require('express');
 var app = express();
+
 
 app.use(express.static('app'));
 
@@ -10,7 +12,14 @@ app.post('/voorbeeld', function(req, res){
    res.end();
     })
 
-
+app.get('/manifest.appcache', function (req, res){
+    var file = fs.readFileSync('./app/app.manifest');
+    res.writeHead('content-type','text/cache-manifest');
+    res.write(file);
+    res.end();
+});
+    
+    
 app.get('/voorbeeld', function(req, res) {
     setTimeout(function(){
         res.writeHead(200, {"Content-Type": "application/json"});
@@ -21,6 +30,6 @@ app.get('/voorbeeld', function(req, res) {
     
     });
 
-app.listen(1337);
+app.listen(1338);
 
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:1338/');
